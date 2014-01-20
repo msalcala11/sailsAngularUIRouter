35f50934f0c332c7606986cf7298c7b2a481b598
msalcala11 <msalcala11@gmail.com>
2014-01-20 07:52:49 -0500

    added max-age header to userfiles

--- api/controllers/FileController.js
+++ api/controllers/FileController.js
@@ -22,6 +22,8 @@ module.exports = {
   get: function (req, res) {
     console.log(req.path);
     console.log(req.path.substr(1));
+    var maxAge = 365*24*60*60*1000 //Sets the cache to expire a year from now
+    res.setHeader('Cache-Control', "'public, max-age="+maxAge+"'");
     res.sendfile(req.path.substr(1));
   },
 
